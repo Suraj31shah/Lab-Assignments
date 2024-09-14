6b8cbcbe1aabad9a83e77794314cdaaf4f777427
Suraj31shah <suraj0631shah@gmail.com>
2024-09-13 22:50:50 -0700

    Update Assignment-37.c

--- Assignment-37.c
+++ Assignment-37.c
@@ -3,7 +3,7 @@ int main() {
     int n,i;
     printf("Enter a number: ");
     scanf("%d",&n);
-    printf("Even numbers uto %d are:\n",n);
+    printf("Even numbers upto %d are:\n",n);
     for (i=1; i<=n; i++) {
         if (i%2==0) {
             printf("%d,",i);
