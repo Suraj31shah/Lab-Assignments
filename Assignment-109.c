5b46dd7c45708de2f27d339242ecbd2e81209a0c
Suraj31shah <suraj0631shah@gmail.com>
2024-11-20 13:59:16 +0530

    Delete Assignment-109.c

--- Assignment-109.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int n=10;
-    int *ptr;
-    ptr=&n;
-    printf("Value of n before modifying is %d\n",n);
-    *ptr=20;
-    printf("Value of n after modifying is %d",n);
-}
