1a753dde2c317505fa9ad8ec5b978a2552117b14
Suraj31shah <suraj0631shah@gmail.com>
2024-11-20 14:00:02 +0530

    Delete Assignment-91.c

--- Assignment-91.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main() {
-    int n;
-    printf("Enter a number: ");
-    scanf("%d",&n);
-    int *ptr=&n;
-    printf("The square of number %d is %d\n",*ptr,*ptr**ptr);
-    printf("The cube of number %d is %d",*ptr,*ptr**ptr**ptr);
-}
