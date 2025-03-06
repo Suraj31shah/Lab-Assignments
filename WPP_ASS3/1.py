8e8f899d4750dff53574a812f90b945b614a5e70
Suraj31shah <suraj0631shah@gmail.com>
2025-03-07 00:37:12 +0530

    Update 1.py

--- WPP_ASS3/1.py
+++ WPP_ASS3/1.py
@@ -1,22 +1,3 @@
-# def digitalRoot(num) :
-#     sum=0
-#     while num>0 :
-#         rem=num%10
-#         sum+=rem
-#         num//=10
-#     if sum>9 :
-#         num=sum
-#         digitalRoot(num)
-#     else :
-#         print("The digital root of the number is:",sum)
-
-# def main() :
-#     num=int(input("Enter a number: "))
-#     digitalRoot(num)
-
-# if __name__=='__main__' :
-#     main()
-
 def digitalRoot(num) :
     sum=0
     newnum=0
